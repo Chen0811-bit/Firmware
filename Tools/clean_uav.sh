a21cec3915a899c5ff983bace22890c60cbfa238
Moutwa <chen18252215885@163.com>
2022-07-20 19:32:13 +0800

    fix fireware bugs in ubuntu 18

---
+++ Tools/clean_uav.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+rm -r /root/src/Firmware/Tools/sitl_gazebo/models/f450-tmp-*
+rm /root/src/Firmware/posix-configs/SITL/init/lpe/f450-tmp-*
+rm /root/src/Firmware/launch/posix_sitl_multi_tmp.launch
+
